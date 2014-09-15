f2acb42d1a895f1f92a11fb3aa4e71f15dfb1b2e
Kenneth Brown <siegel.zero@gmail.com>
2014-09-14 23:32:49 -0700

    Added more graph tests

--- graphs/test_graphs.py
+++ graphs/test_graphs.py
@@ -12,12 +12,18 @@ class TestGraphs(unittest.TestCase):
     def test_vertices(self):
         graph = random_graph(10, 0.5)
 
+        self.assertTrue(graph.vertices() == range(10))
+        self.assertTrue(graph.vertex_set() == set(range(10)))
+
         self.assertTrue(graph.num_vertices() == 10)
         self.assertTrue(len(graph.graph_dict.keys()) == 10)
 
         new_graph = graph.remove_vertex(0)
         self.assertTrue(new_graph.num_vertices() == 9)
         self.assertTrue(len(new_graph.graph_dict.keys()) == 9)
+
+        self.assertTrue(graph.num_vertices() == 10)
+        self.assertTrue(len(graph.graph_dict.keys()) == 10)
 
         new_graph = graph.remove_vertices([0, 1, 2])
         self.assertTrue(new_graph.num_vertices() == 7)
