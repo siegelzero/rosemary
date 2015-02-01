4221e4e84920e5f2e2635354aa872ea879bdf3fc
Kenneth Brown <siegel.zero@gmail.com>
2015-02-01 00:57:05 -0800

    Reworked continued fraction code

--- number_theory/continued_fractions.py
+++ number_theory/continued_fractions.py
@@ -1,78 +1,121 @@
-################################################################################
-# Algorithms related to Continued Fractions.
-################################################################################
+import itertools
 
 from rosemary.number_theory.core import integer_sqrt
 
 
-def quadratic_expansion(d):
-    """Returns the continued fraction expansion of sqrt(d).
-
-    Input:
-        * d: int (d > 0)
-
-    Returns:
-        * terms: list
-            A list of the terms in the continued fraction expansion.
-
-    Examples:
-        >>> quadratic_expansion(5)
-        [2, 4]
-        >>> quadratic_expansion(31)
-        [5, 1, 1, 3, 5, 3, 1, 1, 10]
-
-    Details:
-        If d > 0 is not a perfect square, then the continued fraction expansion
-        of sqrt(d) is periodic of the form [a0; a1, a2, ..., a2, a1, 2*a0],
-        where a0 = floor(sqrt(d)), the periodic part beginning after the first
-        term. See Theorem 5.10 of "Fundamental Number Theory with Applications" by
-        Mollin for details.
+class QuadraticIrrational(object):
+    """A real number a is a quadratic irrational if it can be written in the
+    form (p + sqrt(d))/q, for some integers p, q != 0, and d > 0, where d is not
+    a perfect square.
     """
-    r = integer_sqrt(d)
+    def __init__(self, d, p=0, q=1):
+        """Initializes a new quadratic irrational (p + sqrt(d))/q.
+        """
+        self.p = p
+        self.q = q
+        self.d = d
 
-    if r*r == d:
-        return [r]
+        if p == 0 and q == 1:
+            self._initialize_sqrt(d)
+        else:
+            self._initialize_quadratic(d, p, q)
 
-    Q = 1
-    P = 0
-    terms = []
+    def __repr__(self):
+        """String representation of self.
+        """
+        if self.p == 0:
+            if self.q == 1:
+                s = "sqrt({})".format(self.d)
+            else:
+                s = "sqrt({})/{}".format(self.d, self.q)
+        else:
+            if self.q == 1:
+                s = "{} + sqrt({})".format(self.q, self.d)
+            else:
+                s = "({} + sqrt({}))/{}".format(self.p, self.d, self.q)
 
-    while True:
-        q = (P + r)//Q
+        return "Continued Fraction Expansion of {}".format(s)
 
-        terms.append(q)
-        # The only terms equal to 2*floor(sqrt(d)) are the last terms in a
-        # period, so we can terminate once this occurs.
-        if q == 2*r:
-            return terms
+    def _initialize_quadratic(self, d, p, q):
+        r = integer_sqrt(d)
 
-        P = q*Q - P
-        Q = (d - P*P)//Q
+        if r*r == d:
+            raise ValueError("d cannot be a perfect square")
 
+        Q = q
+        P = p
+        pq_pairs = {(p, q): 0}
+        partial_quotients = []
 
-def quadratic_convergents(d, number=0):
-    """Returns the continued fraction convergents to sqrt(d).
+        for i in itertools.count(1):
+            q = (P + r)//Q
+            P = q*Q - P
+            Q = (d - P*P)//Q
+            partial_quotients.append(q)
 
-    Input:
-        * d: int (d > 0)
+            if (P, Q) in pq_pairs:
+                j = pq_pairs[(P, Q)]
+                k = i
+                break
+            else:
+                pq_pairs[(P, Q)] = i
 
-    Returns::q
+        self.period_length = k - j
+        self.pre_period = partial_quotients[0:j]
+        self.fundamental_period = partial_quotients[j:]
 
+    def _initialize_sqrt(self, d):
+        """If d > 0 is not a perfect square, then the continued fraction
+        expansion of sqrt(d) is periodic of the form [a0; a1, a2, ..., a2, a1,
+        2*a0], where a0 = floor(sqrt(d)), the periodic part beginning after the
+        first term. See Theorem 5.10 of "Fundamental Number Theory with
+        Applications" by Mollin for details.
+        """
+        r = integer_sqrt(d)
+        if r*r == d:
+            raise ValueError("d cannot be a perfect square")
 
-    Details:
-        See Theorem 1.12 of "Fundamental Number Theory with Applications" by
-        Mollin for details.
-    """
-    expansion = quadratic_expansion(d)
+        Q = 1
+        P = 0
+        terms = []
 
-    a0, a1 = 0, 1
-    b0, b1 = 1, 0
+        while True:
+            q = (P + r)//Q
+            terms.append(q)
+            if q == 2*r:
+                break
+            P = q*Q - P
+            Q = (d - P*P)//Q
 
-    for q in expansion:
-        a2 = q*a1 + a0
-        b2 = q*b1 + b0
+        self.period_length = len(terms) - 1
+        self.pre_period = [r]
+        self.fundamental_period = terms[1:]
 
-        yield (a2, b2)
+    def convergents(self):
+        """Returns the continued fraction convergents to self.
 
-        a0, a1 = a1, a2
-        b0, b1 = b1, b2
+        Returns:
+            * X: generator
+                The values output from this generator are pairs (a, b) representing
+                the convergents a/b to sqrt(d).
+
+        Examples:
+            >>> quadratic_convergents(2, 6)
+            [(1, 1), (3, 2), (7, 5), (17, 12), (41, 29), (99, 70), (239, 169)]
+
+        Details:
+            See Theorem 1.12 of "Fundamental Number Theory with Applications" by
+            Mollin for details.
+        """
+        a0, a1 = 0, 1
+        b0, b1 = 1, 0
+
+        terms = itertools.chain(self.pre_period,
+                                itertools.cycle(self.fundamental_period))
+
+        for q in terms:
+            a2 = q*a1 + a0
+            b2 = q*b1 + b0
+            a0, a1 = a1, a2
+            b0, b1 = b1, b2
+            yield (a2, b2)
---
+++ number_theory/diophantine_equations.py
@@ -0,0 +1,114 @@
+from rosemary.number_theory.continued_fractions import quadratic_convergents
+from rosemary.number_theory.core import integer_sqrt
+from rosemary.number_theory.zn_arithmetic import sqrts_mod_n
+
+import sys
+import itertools
+
+
+def pell_fundamental_solution(D, n=1):
+    if D <= 0 or n not in (1, -1):
+        raise ValueError("pell_fundamental_solution: Must have D > 0 and n in (-1, 1).")
+
+    for (x, y) in quadratic_convergents(D):
+        if x*x - D*y*y == n:
+            yield (x, y)
+
+
+def _pell_general(D, N, one_solution=False):
+    """
+    test with D = 1121311213, N = 11889485036288588
+    """
+    if D < 0 or N < 0:
+        raise ValueError("_pell_general: Must have D > 0 and N > 0")
+
+    # Find fundamental solution to Pell
+    for (t, u) in quadratic_convergents(D):
+        if t*t - D*u*u == 1:
+            break
+
+    # Now find fundamental solution to general Pell
+    fundamental_solutions = []
+    residues = sqrts_mod_n(N, D)
+    L = N*(t - 1)
+
+    for r in residues:
+        for x in itertools.count(r, D):
+            w = (x*x - N)//D
+
+            if w < 0:
+                continue
+            elif 2*D*w > L:
+                break
+
+            y = integer_sqrt(w)
+
+            if y*y == w:
+                if one_solution:
+                    yield (x, y)
+                    return
+
+                fundamental_solutions.append((x, y))
+                (r, s) = (-x, y)
+
+                if (x*r - D*y*s) % N != 0 or (x*s - y*r) % N != 0:
+                    fundamental_solutions.append((r, s))
+
+    minimal_positive_solutions = []
+
+    for (x, y) in fundamental_solutions:
+        if x < 0:
+            (r, s) = (-x, -y)
+            p = r*t + s*u*D
+            q = r*u + s*t
+            minimal_positive_solutions.append((p, q))
+        else:
+            minimal_positive_solutions.append((x, y))
+
+    minimal_positive_solutions.sort()
+
+    if not minimal_positive_solutions:
+        return
+
+    while True:
+        for i in xrange(len(minimal_positive_solutions)):
+            x, y = minimal_positive_solutions[i]
+            yield (x, y)
+            minimal_positive_solutions[i] = (x*t + y*u*D, x*u + y*t)
+
+
+def pivotal(m):
+    for (x, y) in _pell_general(4*m*(m + 1), 4*m**2*(m + 1)**3):
+        if y % (m + 1) == 0 and x % (2*m*(m + 1)) == 0:
+            k2 = y//(m + 1) + m
+            n2 = x//(2*m*(m + 1)) - m - 1
+
+            if k2 % 2 == 0 and n2 % 2 == 0 and k2 >= 2 and n2 >= k2:
+                yield (k2//2, n2//2)
+
+
+def solve(N):
+    sols = set()
+    old = 0
+
+    for m in itertools.count(1):
+        not_changed2 = 0
+        for (k, n) in pivotal(m):
+            if k <= N:
+                if k not in sols:
+                    sols.add(k)
+            else:
+                not_changed2 += 1
+
+            if not_changed2 >= 10:
+                break
+
+        new = sum(sols)
+        if new != old:
+            print new
+            old = new
+
+
+if __name__ == "__main__":
+    N = int(sys.argv[1])
+    print solve(N)
